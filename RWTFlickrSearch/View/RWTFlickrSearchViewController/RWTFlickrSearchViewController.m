d17723e4c276c8cff49637029c43a737215ed9a8
Fabio Hiroki <fabiothiroki@gmail.com>
2016-04-24 12:55:54 -0300

    Remove unused files

---
+++ RWTFlickrSearch/View/RWTFlickrSearchViewController/RWTFlickrSearchViewController.m
@@ -0,0 +1,80 @@
+//
+//  Created by Colin Eberhardt on 13/04/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+#import "RWTFlickrSearchViewController.h"
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import "RWTImgurApiRequest.h"
+#import "RWTCollectionViewCell.h"
+#import "UIImageView+AFNetworking.h"
+
+ static NSString *cellIdentifier = @"cvCell";
+
+@interface RWTFlickrSearchViewController () <UICollectionViewDataSource, UICollectionViewDelegate>
+
+@property (weak, nonatomic) RWTFlickrSearchViewModel *viewModel;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+
+@end
+
+@implementation RWTFlickrSearchViewController
+
+- (instancetype)initWithViewModel:(RWTFlickrSearchViewModel *)viewModel{
+    
+    self = [super init];
+    
+    if (self) {
+        self.viewModel = viewModel;
+    }
+    
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+  
+    [self setupViewController];
+    
+    [self bindViewModel];
+    
+    [[self.viewModel.executeSearch execute:nil] subscribeCompleted:^{
+        [self.collectionView reloadData];
+    }];
+    
+}
+
+#pragma mark - Private methods
+
+- (void)setupViewController{
+    
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+    self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStylePlain target:nil action:nil];
+ 
+    [self.collectionView registerNib:[UINib nibWithNibName:@"RWTCollectionViewCell" bundle:nil] forCellWithReuseIdentifier:cellIdentifier];
+}
+
+- (void)bindViewModel{
+    self.title = self.viewModel.title;
+    
+    RAC([UIApplication sharedApplication], networkActivityIndicatorVisible) = self.viewModel.executeSearch.executing;
+}
+
+#pragma mark - UICollectionView
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath{
+    
+    RWTCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    RWTImgurImageItem *item = self.viewModel.results.data[indexPath.row];
+    
+    [cell.imageView setImageWithURL:[[NSURL alloc] initWithString:item.imageUrl]];
+    
+    return cell;
+}
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section{
+    return [self.viewModel.results.data count];
+}
+
+@end
