0accdd7d022acb7e2fb4b14b6e9cb7c5ce4a72d2
Fabio Hiroki <fabiothiroki@gmail.com>
2016-04-24 20:01:50 -0300

    Added tabbar to show about screen

--- RWTFlickrSearch/RWTAppDelegate.m
+++ RWTFlickrSearch/RWTAppDelegate.m
@@ -10,12 +10,13 @@
 #import "RWTFlickrSearchViewController.h"
 #import "RWTFlickrSearchViewModel.h"
 #import "RWTViewModelServicesImpl.h"
+#import "RWTAboutViewController.h"
 
-@interface RWTAppDelegate ()
+@interface RWTAppDelegate () <UITabBarControllerDelegate>
 
-@property (nonatomic, retain) UINavigationController *navigationController;
-@property (nonatomic, strong) RWTFlickrSearchViewModel *viewModel;
+@property (strong, nonatomic) RWTFlickrSearchViewModel *viewModel;
 @property (strong, nonatomic) RWTViewModelServicesImpl *viewModelServices;
+@property (strong, nonatomic) UITabBarController *tabBarController;
 
 @end
 
@@ -23,23 +24,38 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.navigationController = [UINavigationController new];
-  
-  UIViewController *viewController = [self createInitialViewController];
-  [self.navigationController pushViewController:viewController animated:NO];
+    UINavigationController *searchNavigationController = [self getSearchNavigationController];
+    RWTAboutViewController *aboutViewController = [self getAboutViewController];
+    
+    self.tabBarController = [[UITabBarController alloc] init];
+    self.tabBarController.viewControllers = @[searchNavigationController, aboutViewController];
 
-  self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-  self.window.rootViewController = self.navigationController;
-  [self.window makeKeyAndVisible];
-  return YES;
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = self.tabBarController;
+    [self.window makeKeyAndVisible];
+    return YES;
 }
 
 - (UIViewController *)createInitialViewController {
 
     self.viewModelServices = [RWTViewModelServicesImpl new];
     self.viewModel = [[RWTFlickrSearchViewModel alloc] initWithServices:self.viewModelServices];
-    
+
     return [[RWTFlickrSearchViewController alloc] initWithViewModel:self.viewModel];
+}
+
+- (UINavigationController *)getSearchNavigationController{
+    
+    UIViewController *viewController = [self createInitialViewController];
+    UINavigationController *navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+    
+    return navigationController;
+}
+
+- (RWTAboutViewController*)getAboutViewController{
+    
+    return [RWTAboutViewController new];
+    
 }
 
 @end
---
+++ RWTFlickrSearch/View/RWTAboutViewController/RWTAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWTAboutViewController.h
+//  RWTFlickrSearch
+//
+//  Created by Indigo on 4/24/16.
+//  Copyright © 2016 Colin Eberhardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWTAboutViewController : UIViewController
+
+@end
