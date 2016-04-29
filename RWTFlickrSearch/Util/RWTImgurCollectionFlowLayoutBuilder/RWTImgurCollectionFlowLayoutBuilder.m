f2c51a1a5ce155a72762249f4dd400d0addd7e61
Fabio Hiroki <fabiothiroki@gmail.com>
2016-04-28 23:17:35 -0300

    Collection flow builder

---
+++ RWTFlickrSearch/Util/RWTImgurCollectionFlowLayoutBuilder/RWTImgurCollectionFlowLayoutBuilder.m
@@ -0,0 +1,26 @@
+//
+//  RWTImgurCollectionFlowLayoutBuilder.m
+//  RWTFlickrSearch
+//
+//  Created by Indigo on 4/28/16.
+//  Copyright © 2016 Colin Eberhardt. All rights reserved.
+//
+
+#import "RWTImgurCollectionFlowLayoutBuilder.h"
+
+
+@implementation RWTImgurCollectionFlowLayoutBuilder
+
+- (instancetype)initWithViewType:(RWTImgurCollectionViewLayoutType)viewType {
+    
+    self = [super init];
+    
+    if (self) {
+        
+        
+        
+    }
+    return self;
+}
+
+@end
