a77fe2af11d6c3f81ea194ac9c5029ea43cd127b
Fabio Hiroki <fabiothiroki@gmail.com>
2016-05-01 15:53:08 -0300

    Window model class

---
+++ RWTFlickrSearch/Model/RWTImgurWindow/RWTImgurWindow.h
@@ -0,0 +1,25 @@
+//
+//  RWTImgurWindow.h
+//  RWTFlickrSearch
+//
+//  Created by Indigo on 5/1/16.
+//  Copyright © 2016 Colin Eberhardt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWTImgurWindow : NSObject
+
+typedef NS_ENUM(NSInteger, RWTImgurWindowType) {
+    RWTImgurWindowTypeDay,
+    RWTImgurWindowTypeWeek,
+    RWTImgurWindowTypeMonth,
+    RWTImgurWindowTypeYear,
+    RWTImgurWindowTypeAll
+};
+
+- (instancetype)initWithWindowType:(RWTImgurWindowType)windowType;
+- (RWTImgurWindowType)windowType;
+- (NSString*)prettyName;
+
+@end
